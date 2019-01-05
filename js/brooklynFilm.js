ade5815661125b630713cd94e60b496f1e1d4191
chrishj59 <carshaltonm59@gmail,com>
2019-01-05 16:00:07 +0000

    Final update before review

---
+++ js/brooklynFilm.js
@@ -0,0 +1,19 @@
+
+$(function () {
+  $('header a').on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000, function(){
+      window.location.hash = hash;
+    });
+  });
+});
+
+$(function(){
+var navMain = $("#filmNavbar");
+navMain.on("click", "a", null, function () {
+   navMain.collapse('hide');
+});
+});
